023d3fcf658871ca3321c829abc32db9234963dd
Lincelot <shd00fah@hotmail.com>
2024-05-02 20:07:54 +0800

    FEAT - 新增futureHandler

---
+++ lib/domain/result/future_handler.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_practice_services/domain/failure/failure.dart';
+
+Future<Either<Failure, T>> futureHandler<T>(Future<T> Function() future) async {
+  try {
+    return right(await future());
+  } on DioException catch (e) {
+    switch (e.error.runtimeType) {
+      case APIException:
+        return left(AIPFailure(e,
+            baseResponse: (e.error as APIException).baseResponse));
+      case HttpConnectException:
+        return left(HttpConnectFailure(e));
+      case APITimeoutException:
+        return left(APITimeoutFailure(e));
+      default:
+        return left(DioFailure(e));
+    }
+  } catch (e) {
+    return left(RuntimeFailure(Exception('$e')));
+  }
+}
