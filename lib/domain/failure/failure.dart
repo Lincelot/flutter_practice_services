317af8be9c46b5fd2807b16b10d07e13c62145e2
Lincelot <shd00fah@hotmail.com>
2024-05-02 19:44:28 +0800

    ADD - 新增自定義錯誤

---
+++ lib/domain/failure/failure.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_practice_services/domain/failure/base_response.dart';
+
+abstract class Failure extends Equatable {
+  final Exception ex;
+
+  const Failure(this.ex);
+
+  @override
+  List<Object> get props => [ex];
+}
+
+class RuntimeFailure extends Failure {
+  const RuntimeFailure(Exception ex) : super(ex);
+}
+
+class DioFailure extends Failure {
+  final DioException dioException;
+
+  const DioFailure(this.dioException) : super(dioException);
+}
+
+class HttpConnectException implements Exception {
+  HttpConnectException();
+}
+
+class HttpConnectFailure extends Failure {
+  final DioException dioException;
+
+  const HttpConnectFailure(this.dioException) : super(dioException);
+}
+
+class APITimeoutException implements Exception {
+  APITimeoutException();
+}
+
+class APITimeoutFailure extends Failure {
+  final DioException dioException;
+
+  const APITimeoutFailure(this.dioException) : super(dioException);
+}
+
+class APIException implements Exception {
+  final BaseResponse baseResponse;
+
+  APIException(this.baseResponse);
+}
+
+class AIPFailure extends Failure {
+  final DioException dioException;
+  final BaseResponse? baseResponse;
+
+  const AIPFailure(this.dioException, {this.baseResponse})
+      : super(dioException);
+}
